96721a6b11f4620bb3557ce7f36ff69f3168365e
xiaolitongxue666 <leonpa1987@gmail.com>
2020-02-18 09:01:25 +0800

    opencv hisiv200 static lib

---
+++ build/CMakeFiles/opencv_hisi_test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xiaoli/Code/OpenCV/opencv_hisi_test/src/opencv_test.cpp" "/home/xiaoli/Code/OpenCV/opencv_hisi_test/build/CMakeFiles/opencv_hisi_test.dir/src/opencv_test.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/xiaoli/Code/OpenCV/3.1.0/install/include"
+  "/home/xiaoli/Code/OpenCV/3.1.0/install/include/opencv"
+  "/home/xiaoli/Code/OpenCV/3.1.0/install/include/opencv2"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/opencv_hisi_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/opencv_hisi_test.dir/src/opencv_test.cpp.o"
+  "opencv_hisi_test.pdb"
+  "opencv_hisi_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/opencv_hisi_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
